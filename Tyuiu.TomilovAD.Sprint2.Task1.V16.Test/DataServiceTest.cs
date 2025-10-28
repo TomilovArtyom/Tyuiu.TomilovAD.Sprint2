5aafa13a03765653bb08b5f6ee43eb1c16dc7ae0
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-28 21:34:31 +0500

    Task 1

---
+++ Tyuiu.TomilovAD.Sprint2.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.TomilovAD.Sprint2.Task1.V16.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint2.Task1.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int a = 145;
+            int b = 716;
+            int c = 144;
+            int d = 137;
+
+            bool[] res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[] { true, true, true, false, true, false };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint2.Task1.V16/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.TomilovAD.Sprint2.Task1.V16.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint1.Task3.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #                                                                *");
+            Console.WriteLine("* Задание #                                                               *");
+            Console.WriteLine("* Вариант #                                                               *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+
+            int a;
+            int b;
+            int c;
+            int d;
+
+            Console.WriteLine("Введите значение A:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение B:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение C:");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение D:");
+            d = Convert.ToInt32(Console.ReadLine());
+
+            bool[] res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
