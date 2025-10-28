62916a5bc7e84d4e6726eb365a5c1db9750ca74d
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-28 20:31:29 +0500

    Add project files.

---
+++ Tyuiu.TomilovAD.Sprint2.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.TomilovAD.Sprint2.Task0.V6.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint2.Task0.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 107;
+            int y = 754;
+            bool[] res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[] { false, true, true, false, true, false };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TomilovAD.Sprint2.Task0.V6/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.TomilovAD.Sprint2.Task0.V6.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint2.Task0.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            int y;
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            bool[] res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
