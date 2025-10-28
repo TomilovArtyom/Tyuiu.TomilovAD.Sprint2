c58abc2af885c64bcaf302beefa772c250fc7cd0
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-28 23:04:49 +0500

    Task 3 test

---
+++ Tyuiu.TomilovAD.Sprint2.Task3.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.TomilovAD.Sprint2.Task3.V3.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint2.Task3.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double x = 0;
+            DataService ds = new DataService();
+            double wait = 0.167;
+            double res = ds.Calculate(x);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
