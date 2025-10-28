1a43c0f8f585ddc2032fd15f300e412e880beade
Артём Томилов <Tyuiu.TomilovAD@outlook.com>
2025-10-28 23:00:15 +0500

    Task 3

---
+++ Tyuiu.TomilovAD.Sprint2.Task3.V3.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.TomilovAD.Sprint2.Task3.V3.Lib
+{
+    public class DataService : ISprint2Task3V3
+    {
+        public double Calculate(double x)
+        {
+            if (x > 1)
+            {
+                return Math.Round(Math.Pow(x * ((x + 1) / (x - 1)), x), 3);
+            }
+            else
+            {
+                if (x == 0)
+                {
+                    return Math.Round((Math.Pow(x, 4) - Math.Pow(Math.Cos(x), 5) + 3) / (Math.Pow(x, 2) - Math.Pow(Math.Sin(x), 2) + 12), 3);
+                }
+                else
+                {
+                    if ((-7 < x) && (x < 0))
+                    {
+                        return Math.Round((1 + (Math.Sqrt(x + 3) / Math.Pow(x, 2))), 3);
+                    }
+                    else
+                    {
+                        if (x < -7)
+                        {
+                            return Math.Round(Math.Pow(x, 2) + 10 * x - (1 / x), 3);
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                }
+            }
+            }    
+        }
+    }
+
---
+++ Tyuiu.TomilovAD.Sprint2.Task3.V3/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.TomilovAD.Sprint2.Task3.V3.Lib;
+
+namespace Tyuiu.TomilovAD.Sprint2.Task3.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил Томилов А. Д. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Томилов А. Д. | ИСТНб-25-1                                    *");
+            Console.WriteLine("***************************************************************************");
+
+            double x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x));
+            Console.ReadLine();
+        }
+    }
+}
